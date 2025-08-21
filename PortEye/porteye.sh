36dd6575983908e0b9f68b9dfed4a6344a312f08
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-08-21 21:10:33 +0530

    Update porteye.sh

--- PortEye/porteye.sh
+++ PortEye/porteye.sh
@@ -7,33 +7,21 @@ YELLOW="\e[0;33m"
 BLUE="\e[0;34m"
 
 # check if the user provide 3 arguments 
-
 if [[ $# -ne 3 ]]; then
-
-echo -e "${YELLOW}porteye.sh <targetip> <startingport> <endingport>${NC}"
-exit 1
-
+    echo -e "${YELLOW}Usage: porteye.sh <targetip> <startingport> <endingport>${NC}"
+    exit 1
 fi
+
+# global array for open ports
+result_array=()
 
 # user press Ctrl+C the script will stop
-
 function handle_interrupt() {
-
- # print the discovered open ports before exiting
-if [[ ${#result_array[@]} -ne 0 ]]; then
-echo ""
-echo -e "${YELLOW} Discoverd Open Ports ${NC}\n"
-i=1
-while((i<$count))
-do
-
-echo -e "$GREEN${result_array[$i]}$NC"
-
-((i++))
-
-done
-
-fi
+    if [[ ${#result_array[@]} -ne 0 ]]; then
+        echo ""
+        echo -e "${YELLOW}Discovered Open Ports before exit:${NC}\n"
+        printf "%b\n" "${result_array[@]}"
+    fi
 
     echo ""
     echo -e "${RED}\nKeyboard interrupt (Ctrl+C) detected. Exiting the script...${NC}"
@@ -43,11 +31,10 @@ fi
 # Trap SIGINT (Ctrl+C)
 trap handle_interrupt SIGINT
 
-
 display_banner() {
-clear
-echo -e "${GREEN}"
-cat << "EOF" 
+    clear
+    echo -e "${GREEN}"
+    cat << "EOF" 
   
   _____           _   ______           
  |  __ \         | | |  ____|          
@@ -61,59 +48,41 @@ cat << "EOF"
                     Developer: Sreeraj
 
 EOF
-echo -e "${NC}${YELLOW}* GitHub: https://github.com/s-r-e-e-r-aj${NC}\n"
+    echo -e "${NC}${YELLOW}* GitHub: https://github.com/s-r-e-e-r-aj${NC}\n"
 }
 
 scanner() {
+    ipaddress=$1
+    startingport=$2
+    endingport=$3
 
-result_array=();
-count=1
+    echo -e "${BLUE}Scanning ports ${startingport} to ${endingport} on host ${ipaddress} ${NC}\n"
 
-ipaddress=$1
-startingport=$2
-endingport=$3
+    for (( port=startingport; port<=endingport; port++ )); do
+        timeout 2 bash -c "echo > /dev/tcp/$ipaddress/$port" &>/dev/null
+        if [[ $? -eq 0 ]]; then
+            echo -e "$GREEN port $port is open at host $ipaddress $NC"
+            result_array+=("port $port is open at host $ipaddress")
+        else
+            echo -e "$RED port $port is closed at host $ipaddress $NC"
+        fi
+    done
 
-echo -e "${BLUE}Scanning Port ${startingport} To ${endingport} At Host ${ipaddress} ${NC}\n"
+    echo ""
 
-for(( port=startingport;port<=endingport;port++))
-do
+    # print the discovered open ports
+    if [[ ${#result_array[@]} -ne 0 ]]; then
+        echo -e "${YELLOW}Discovered Open Ports:${NC}\n"
+        printf "%b\n" "${result_array[@]}"
 
-timeout 2  bash -c "echo > /dev/tcp/$ipaddress/$port" &> /dev/null
-
-if [ $? -eq 0 ]; then
-
-echo -e  "$GREEN port $port is open at host $ipaddress$NC"
-result_array[$count]="port $port is open at host $ipaddress" 
-((count++))
-else
-
-echo -e "$RED port $port is closed at host $ipaddress$NC"
-
-fi
-
-
-done
-
-echo ""
-
-# print the discovered open ports
-if [[ ${#result_array[@]} -ne 0 ]]; then
-
-echo -e "${YELLOW}Discovered Open Ports ${NC}\n"
-
-i=1
-while((i<$count))
-do
-
-echo -e "$GREEN${result_array[$i]}$NC"
-
-((i++))
-
-done
-
-fi
-
+        # save to file
+        logfile="scan_results_$(date +%F_%H-%M-%S).txt"
+        printf "%s\n" "${result_array[@]}" > "$logfile"
+        echo -e "\n${BLUE}Results saved to $logfile${NC}"
+    else
+        echo -e "${RED}No open ports found.${NC}"
+    fi
 }
 
 display_banner
-scanner $1 $2 $3
+scanner "$1" "$2" "$3"
